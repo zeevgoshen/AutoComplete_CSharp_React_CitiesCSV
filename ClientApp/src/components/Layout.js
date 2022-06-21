48a528e9091fb7fe08c672e8936ad8b16c795a86
Zeev <zeevgoshen@gmail.com>
2022-06-21 13:11:58 +0300

    removed unused file, added css file

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
+
 
 export class Layout extends Component {
   static displayName = Layout.name;
@@ -8,7 +8,6 @@ export class Layout extends Component {
   render () {
     return (
       <div>
-        <NavMenu />
         <Container>
           {this.props.children}
         </Container>
