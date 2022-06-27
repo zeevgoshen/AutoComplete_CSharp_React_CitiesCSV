54f01350d6668f13dac992ec6d14d1613f4b8b52
Zeev <zeevgoshen@gmail.com>
2022-06-27 14:49:12 +0300

    client refactored to service

--- ClientApp/src/components/AutoComplete.js
+++ ClientApp/src/components/AutoComplete.js
@@ -1,37 +1,14 @@
 ﻿import React, { useMemo, useState, useEffect } from 'react';
-import axios from 'axios';
 import debounce from 'lodash.debounce';
+import { useSuggestions } from '../services/suggestions.service'
 
 import './AutoComplete.css'
 
 const AutoComplete = () => {
 
     const [filterText, setFilter] = useState('');
-    const [cities, setCities] = useState([]);
-
     const [inputValue, setInputValue] = useState('');
-
-
-    const getFilteredCities = async () => {
-
-        const options = {
-            headers: { "content-type": "application/json" }
-        }
-
-        await axios
-            .post('suggestions', { "text": filterText }, options)
-            .then((response) => setCities(response.data))
-            .catch((err) => console.log(err));
-    };
-
-    useMemo(() => {
-        // no search text in the search textbox
-        // show all issues
-        if (filterText.length !== 0) {
-            getFilteredCities();
-        }
-    }, [filterText]);
-
+    const [cities, setCities] = useSuggestions(filterText);
 
     const handleChange = (e) => {
         setInputValue();
---
+++ ClientApp/src/services/suggestions.service.js
@@ -0,0 +1,38 @@
+﻿import { useState, useEffect } from "react";
+import axios from 'axios'
+
+export function useSuggestions(filterText) {
+
+
+    const [cities, setCities] = useState([]);
+
+    const getFilteredCities = async () => {
+
+        const options = {
+            headers: { "content-type": "application/json" }
+        }
+
+        await axios
+            .post('suggestions', { "text": filterText }, options)
+            .then((response) => setCities(response.data))
+            .catch((err) => console.log(err));
+    };
+
+    useEffect(() => {
+         
+
+        getFilteredCities(filterText).then((response) => {
+            console.log('loaded cities');
+            setCities(response.data.issues);
+            
+             
+        }).catch((e) => {
+            console.log(e.message);
+             
+        });
+    }, [filterText]);
+
+    return [cities, setCities, getFilteredCities];
+};
+
+
