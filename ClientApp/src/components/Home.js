262f2e0d442aff13a3a3193c87340f6f66b0734e
Zeev <zeevgoshen@gmail.com>
2022-06-27 10:56:31 +0300

    unused imports

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import AutoComplete from './AutoComplete'
 import './AutoComplete.css'
-import axios from "axios";
 
 export default function Home() {
 
